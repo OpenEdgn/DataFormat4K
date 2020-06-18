538fca7213287a5c05d93ad8f8fd630295359cc3
Noob <ExplodingFKL@gmail.com>
2020-06-18 22:18:35 +0800

    add test.

--- core/src/test/kotlin/com/github/openEdgn/dataFormat4K/prop/BasePropertiesTest.kt
+++ core/src/test/kotlin/com/github/openEdgn/dataFormat4K/prop/BasePropertiesTest.kt
@@ -3,6 +3,10 @@ package com.github.openEdgn.dataFormat4K.prop
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class BasePropertiesTest{
+class BasePropertiesTest{
 
+    @Test
+    fun test(): Unit {
+
+    }
 }
