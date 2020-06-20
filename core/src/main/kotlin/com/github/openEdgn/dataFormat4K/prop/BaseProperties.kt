57c0af507ffe9f85b504e13bc9b1c9fab04e8bc9
Noob <ExplodingFKL@gmail.com>
2020-06-20 14:36:41 +0800

    impl IDataProp .

--- core/src/main/kotlin/com/github/openEdgn/dataFormat4K/prop/BaseProperties.kt
+++ core/src/main/kotlin/com/github/openEdgn/dataFormat4K/prop/BaseProperties.kt
@@ -13,102 +13,70 @@ abstract class BaseProperties : IDataProperties {
 
 
     override fun putByte(key: String, value: Byte) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun putInt(key: String, value: Int) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun putLong(key: String, value: Long) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun putShort(key: String, value: Short) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun putDouble(key: String, value: Double) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun putBoolean(key: String, value: Boolean) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun putChar(key: String, value: Char) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun putString(key: String, value: String) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
     override fun <T : Serializable> putObject(key: String, value: T) {
-        TODO("Not yet implemented")
+        set(key, value)
     }
 
-    override fun getByte(key: String): Byte? {
-        TODO("Not yet implemented")
-    }
+    override fun getByte(key: String): Byte? = get(key)
 
-    override fun getInt(key: String): Int? {
-        TODO("Not yet implemented")
-    }
+    override fun getInt(key: String): Int? = get(key)
 
-    override fun getShort(key: String): Short? {
-        TODO("Not yet implemented")
-    }
+    override fun getShort(key: String): Short? = get(key)
 
-    override fun getDouble(key: String): Double? {
-        TODO("Not yet implemented")
-    }
+    override fun getDouble(key: String): Double? = get(key)
 
-    override fun getChar(key: String): Char? {
-        TODO("Not yet implemented")
-    }
+    override fun getChar(key: String): Char? = get(key)
 
-    override fun getString(key: String): String? {
-        TODO("Not yet implemented")
-    }
+    override fun getString(key: String): String? = get(key)
 
-    override fun getBoolean(key: String): Boolean? {
-        TODO("Not yet implemented")
-    }
+    override fun getBoolean(key: String): Boolean? = get(key)
 
-    override fun <T : Serializable> getObject(key: String): T? {
-        TODO("Not yet implemented")
-    }
+    override fun <T : Serializable> getObject(key: String): T? = get(key)
 
-    override fun getByteOrDefault(key: String, defaultValue: Byte): Byte {
-        TODO("Not yet implemented")
-    }
+    override fun getByteOrDefault(key: String, defaultValue: Byte): Byte = getValueOrDefault(key, defaultValue)
 
-    override fun getIntOrDefault(key: String, defaultValue: Int): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getIntOrDefault(key: String, defaultValue: Int): Int = getValueOrDefault(key, defaultValue)
 
-    override fun getShortOrDefault(key: String, defaultValue: Short): Short {
-        TODO("Not yet implemented")
-    }
+    override fun getShortOrDefault(key: String, defaultValue: Short): Short = getValueOrDefault(key, defaultValue)
 
-    override fun getDoubleOrDefault(key: String, defaultValue: Double): Double {
-        TODO("Not yet implemented")
-    }
+    override fun getDoubleOrDefault(key: String, defaultValue: Double): Double = getValueOrDefault(key, defaultValue)
 
-    override fun getCharOrDefault(key: String, defaultValue: Char): Char {
-        TODO("Not yet implemented")
-    }
+    override fun getCharOrDefault(key: String, defaultValue: Char): Char = getValueOrDefault(key, defaultValue)
 
-    override fun getStringOrDefault(key: String, defaultValue: String): String {
-        TODO("Not yet implemented")
-    }
+    override fun getStringOrDefault(key: String, defaultValue: String): String = getValueOrDefault(key, defaultValue)
 
-    override fun getBooleanOrDefault(key: String, defaultValue: Boolean): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun getBooleanOrDefault(key: String, defaultValue: Boolean): Boolean = getValueOrDefault(key, defaultValue)
 
-    override fun <T : Serializable> getObjectOrDefault(key: String, defaultValue: T): T {
-        TODO("Not yet implemented")
-    }
+    override fun <T : Serializable> getObjectOrDefault(key: String, defaultValue: T): T = getValueOrDefault(key, defaultValue)
 }
