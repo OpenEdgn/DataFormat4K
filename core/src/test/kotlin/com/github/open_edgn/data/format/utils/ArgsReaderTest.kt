09305b1bfa40b6b7e4b236c3bea7fefc35c381b5
Noob <ExplodingFKL@gmail.com>
2020-07-07 16:50:58 +0800

    auto save.

--- core/src/test/kotlin/com/github/open_edgn/data/format/utils/ArgsReaderTest.kt
+++ core/src/test/kotlin/com/github/open_edgn/data/format/utils/ArgsReaderTest.kt
@@ -4,12 +4,17 @@ import com.github.open_edgn.data.format.ArgsItem
 import org.junit.jupiter.api.Test
 import java.io.File
 
+import org.junit.jupiter.api.Assertions.*
+
 internal class ArgsReaderTest {
     @Test
     fun test() {
-        val argsReader = ArgsReader(arrayOf("--config", "%{user.home}"), ArgsLoader::class,Data::class)
-        println(argsReader.getArgsBean(ArgsLoader::class))
-        println(argsReader.getArgsBean(Data::class))
+        val argsReader = ArgsReader(arrayOf("--config", "%{user.home}"), ArgsLoader::class, Data::class)
+        val message = argsReader.getArgsBean(ArgsLoader::class)
+        assertEquals(message.config, StringFillUtils.fillFormSystemProp("%{user.home}"))
+        assertEquals(message.configPath.absolutePath, StringFillUtils.fillFormSystemProp("%{user.dir}"))
+        val argsBean = argsReader.getArgsBean(Data::class)
+        assertEquals(argsBean.logger.absolutePath,StringFillUtils.fillFormSystemProp("%{JAVA_HOME}"))
 
     }
 
@@ -21,9 +26,10 @@ internal class ArgsReaderTest {
             @ArgsItem("false")
             val debug: Boolean
     )
-    class Data{
+
+    class Data {
         @ArgsItem("%{JAVA_HOME}")
-        lateinit var logger:File
+        lateinit var logger: File
         override fun toString(): String {
             return "Data(logger='$logger')"
         }
